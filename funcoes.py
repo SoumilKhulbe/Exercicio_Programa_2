405ad40d1971e40df1d009f77efe2f4a4fa9d132
Soumil <soumillk@al.insper.edu.br>
2026-05-01 11:15:37 -0300

    Exercicio 12 revisao 7

--- funcoes.py
+++ funcoes.py
@@ -161,9 +161,13 @@ def faz_jogada(dados, categoria, cartela_de_pontos):
     simples = [1, 2, 3, 4, 5, 6]
     avancada = ["cinco_iguais", "full_house", "quadra", "sem_combinacao", "sequencia_alta", "sequencia_baixa"]
     if int(categoria) in simples:
-        cartela_de_pontos["regra_simples"][categoria] = calcula_pontos_regra_simples(dados)
-    elif int(categoria) in avancada:
-        cartela_de_pontos["regra_avancada"][categoria] = calcula_pontos_regra_avancada(dados)
+        a = calcula_pontos_regra_simples(dados)
+        pontos = a[categoria]
+        cartela_de_pontos["regra_simples"][categoria] = pontos
+    elif (categoria) in avancada:
+        b = calcula_pontos_regra_avancada(dados)
+        pontos = b[categoria] 
+        cartela_de_pontos["regra_avancada"][categoria] = pontos
     
     return cartela_de_pontos
 

0dff94ac5b9cf4c4e5ac375ecbdc10f35bd7dafe
Murilo <murilopf10@gmail.com>
2026-05-01 01:38:25 -0300

    Exercicio 12

--- funcoes.py
+++ funcoes.py
@@ -139,3 +139,15 @@ def calcula_pontos_quina(lista):
         return 50
        
     return 0
+
+
+
+
+def faz_jogada(dados, categoria, cartela_de_pontos):
+    if categoria in cartela_de_pontos['regra_simples']:
+        pontos = calcula_pontos_regra_simples(dados, categoria)
+        cartela_de_pontos['regra_simples'][categoria] = ponto
+    elif categoria in cartela_de_pontos['regra_avancada']:
+        pontos = calcula_pontos_regra_avancada(dados, categoria)
+        cartela_de_pontos['regra_avancada'][categoria] = pontos 
+    return cartela_de_pontos

977395b81be16707e27ec0c6f0d4ca550f70c156
Soumil <soumillk@al.insper.edu.br>
2026-05-01 23:12:30 -0300

    Exercicio 13 revisao 7

--- programa.py
+++ programa.py
@@ -83,6 +83,8 @@ for rodada in range(12):
             imprime_cartela(cartela)
             print(f"Dados rolados: {rolados}")
             print(f"Dados guardados: {guardados}")
+            print("Digite 1 para guardar um dado, 2 para remover um dado, 3 para rerrolar, 4 para ver a cartela ou 0 para marcar a pontuação:")
+
             continue
         
         elif a == "0":
